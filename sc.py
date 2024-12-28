adac378cf930c9e5cfd348194e4e584a76af057e
alirezanavi <a.navehkesh@aprofile.ir>
2024-12-28 15:19:51 +0330

    fixed cointel

--- sc.py
+++ sc.py
@@ -37,14 +37,93 @@ import requests
 # # بستن مرورگر
 # driver.quit()
 
-sample = requests.get(url= "https://cryptopotato.com/crypto-news/")
+# sample = requests.get(url= "https://cryptopotato.com/crypto-news/")
 
-print(sample.status_code)
-if sample.status_code == 200:
-    print("success!")
-site_text = sample.text
+# print(sample.status_code)
+# if sample.status_code == 200:
+#     print("success!")
+# site_text = sample.text
 
-soup = BeautifulSoup(site_text, "html.parser")
+# soup = BeautifulSoup(site_text, "html.parser")
 
-product_title = soup.find('h1', class_ ='entry-title')
-print(product_title)
+# product_title = soup.find('h1', class_ ='entry-title')
+# print(product_title)
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+# تنظیمات ChromeDriver
+chrome_options = Options()
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+# chrome_options.add_argument('--headless')  # در صورت نیاز فعال کنید
+
+# مسیر ChromeDriver
+driver_path = r"C:\chromedriver-win64\chromedriver.exe"
+
+# شروع WebDriver
+driver = webdriver.Chrome(service=Service(driver_path), options=chrome_options)
+
+try:
+    # باز کردن صفحه اصلی Google
+    driver.get("******************************")
+
+    # یافتن باکس جستجو و ارسال متن
+    # search_box = WebDriverWait(driver, 10).until(
+    #     EC.element_to_be_clickable((By.NAME, "q"))
+    # )
+    # search_box.send_keys("Selenium")
+    # search_box.send_keys(Keys.RETURN)
+
+    # اضافه کردن تاخیر برای بارگذاری نتایج
+    time.sleep(5)
+
+    market_news_items = driver.find_elements(By.CSS_SELECTOR, "ul.market-news__list li.market-news__item")
+
+    # حلقه برای استخراج اطلاعات از هر آیتم
+    for item in market_news_items:
+        # استخراج عنوان و لینک
+        title_element = item.find_element(By.CSS_SELECTOR, ".post-card-inline__title-link")
+        title = title_element.text
+        link = title_element.get_attribute("href")
+
+        # استخراج توضیحات (متن کوتاه)
+        description = item.find_element(By.CSS_SELECTOR, ".post-card-inline__text").text
+
+        # چاپ اطلاعات
+        print("Title:", title)
+        print("Link:", link)
+        print("Description:", description)
+        print("-" * 50)
+
+
+    time.sleep(5)
+
+
+
+finally:
+    # بستن مرورگر
+    driver.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
