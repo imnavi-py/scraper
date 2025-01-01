6df8be7b072a64b5fd51525dff7878e6cee16672
alirezanavi <a.navehkesh@aprofile.ir>
2025-01-01 09:02:31 +0330

    test

--- sc.py
+++ sc.py
@@ -1,61 +1,7 @@
-# from requests import request
-from bs4 import BeautifulSoup
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-import requests
-
-# # تنظیمات ChromeDriver
-# chrome_options = Options()
-# chrome_options.add_argument('--headless')  # اجرای مرورگر در پس‌زمینه
-# chrome_options.add_argument('--disable-gpu')  # غیرفعال کردن GPU
-# chrome_options.add_argument('--no-sandbox')  # رفع مشکلات در محیط‌های محدود
-# chrome_options.add_argument('--disable-dev-shm-usage')  # رفع مشکلات در محیط‌های محدود
-
-# # مسیر ChromeDriver
-# driver_path = r"C:\chromedriver-win64\chromedriver.exe"
-
-# # شروع WebDriver
-# driver = webdriver.Chrome(service=Service(driver_path), options=chrome_options)
-
-# # باز کردن صفحه مورد نظر
-# url = "https://www.cointelegraph.com/tags/nft"
-# driver.get(url)
-
-# # انتظار برای بارگذاری کامل عنوان
-# try:
-#     title_tag = driver.find_element(By.XPATH, "//h1[contains(@class, 'tag-page__title')]")
-#     print("Page Title:", title_tag.text)
-
-#     print("Page Title:", title_tag.text)
-# except Exception as e:
-#     print("Error:", e)
-
-# # بستن مرورگر
-# driver.quit()
-
-# sample = requests.get(url= "https://cryptopotato.com/crypto-news/")
-
-# print(sample.status_code)
-# if sample.status_code == 200:
-#     print("success!")
-# site_text = sample.text
-
-# soup = BeautifulSoup(site_text, "html.parser")
-
-# product_title = soup.find('h1', class_ ='entry-title')
-# print(product_title)
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
 
 # تنظیمات ChromeDriver
@@ -72,23 +18,35 @@ driver_path = r"C:\chromedriver-win64\chromedriver.exe"
 driver = webdriver.Chrome(service=Service(driver_path), options=chrome_options)
 
 try:
-    # باز کردن صفحه اصلی Google
-    driver.get("********************")
+    # باز کردن صفحه اصلی
+    driver.get("https://cointelegraph.com/markets")
 
-    # یافتن باکس جستجو و ارسال متن
-    # search_box = WebDriverWait(driver, 10).until(
-    #     EC.element_to_be_clickable((By.NAME, "q"))
-    # )
-    # search_box.send_keys("Selenium")
-    # search_box.send_keys(Keys.RETURN)
-
-    # اضافه کردن تاخیر برای بارگذاری نتایج
+    # اضافه کردن تاخیر برای بارگذاری صفحه
     time.sleep(5)
 
-    market_news_items = driver.find_elements(By.CSS_SELECTOR, "ul.market-news__list li.market-news__item")
+    # پیدا کردن تمام لینک‌های 'markets-more__link'
+    links = driver.find_elements(By.CSS_SELECTOR, '.markets-more__link')
 
-    # حلقه برای استخراج اطلاعات از هر آیتم
+    # اسکرول به لینک اول (اگر بیشتر از یک لینک پیدا کردیم)
+    if len(links) > 1:
+        driver.execute_script("arguments[0].scrollIntoView(true);", links[1])
+
+    # اضافه کردن تاخیر برای بارگذاری محتوای جدید
+    time.sleep(2)
+
+    # کلیک روی دومین لینک (ایندکس 1)
+    if len(links) > 1:
+        links[1].click()
+
+    # اضافه کردن تاخیر برای بارگذاری محتوای جدید
+    time.sleep(5)
+
+    market_news_items = driver.find_elements(By.CSS_SELECTOR, ".market-news__item")
+    count = 0
     for item in market_news_items:
+        if count >= 10:
+            break
+
         # استخراج عنوان و لینک
         title_element = item.find_element(By.CSS_SELECTOR, ".post-card-inline__title-link")
         title = title_element.text
@@ -100,6 +58,7 @@ try:
         # استخراج تصویر (URL تصویر)
         img_element = item.find_element(By.CSS_SELECTOR, ".lazy-image__img")
         img_url = img_element.get_attribute("src")
+
         # چاپ اطلاعات
         print("Title:", title)
         print("Link:", link)
@@ -107,27 +66,9 @@ try:
         print("Image URL:", img_url)
         print("-" * 50)
 
-
-    time.sleep(5)
-
-
+        # افزایش شمارنده
+        count += 1
 
 finally:
     # بستن مرورگر
     driver.quit()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
